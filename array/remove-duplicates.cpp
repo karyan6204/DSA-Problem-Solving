b49a2373444c4f17110d08857ac8ef72f4b888c0
Kumar Aryan <aryans6204@gmail.com>
2023-09-23 17:28:14 +0530

    remove duplicates from an array

---
+++ array/remove-duplicates.cpp
@@ -0,0 +1,20 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+int removeDuplicates(vector<int> &arr, int n) {
+	// Write your code here.
+	int count = 1;
+	for(int i=1; i<n; i++){
+		if(arr[i]!=arr[i-1]){
+			count++;
+		}
+	}
+	return count;
+}
+
+int main(){
+    int n; cin>>n;
+    vector<int> arr(n);
+    for(int i=0; i<n; i++) cin>>arr[i];
+    cout<<removeDuplicates(arr, n)<<endl;
+}
