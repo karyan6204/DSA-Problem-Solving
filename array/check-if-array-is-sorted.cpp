4671123a833c9576273995ee6e0696fb097c2e7b
Kumar Aryan <aryans6204@gmail.com>
2023-09-23 17:08:44 +0530

    check if an arrya is sorted or not

---
+++ array/check-if-array-is-sorted.cpp
@@ -0,0 +1,32 @@
+#include <bits/stdc++.h> 
+using namespace std;
+
+int isSorted(int n, vector<int> a) {
+    // Write your code here.
+    int flag = 0;
+    for(int i=0; i<n-1; i++){
+        if(a[i]>a[i+1]){
+            flag = 1;
+            break;
+        }
+    }
+    if(flag){
+        return 0;
+    }else{
+        return 1;
+    }
+}
+
+int main(){
+    int n; cin>>n;
+    vector<int> arr(n);
+    for(int i=0; i<n; i++){
+        cin>>arr[i];
+    }
+    int ans = isSorted(n, arr);
+    if(ans){
+        cout<<"SORTED"<<endl;
+    }else{
+        cout<<"NOT SORTED"<<endl;
+    }
+}
