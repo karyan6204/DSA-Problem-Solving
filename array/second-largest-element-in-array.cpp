7f83e362f634061d2117384996670abe89a8ca53
Kumar Aryan <aryans6204@gmail.com>
2023-09-23 17:00:54 +0530

    second largest and second smallest element in an array

---
+++ array/second-largest-element-in-array.cpp
@@ -0,0 +1,39 @@
+#include <bits/stdc++.h> 
+using namespace std;
+
+vector<int> getSecondOrderElements(int n, vector<int> a) {
+    // Write your code here.
+    int maxi = INT_MIN, mini = INT_MAX;
+    int ans1 = INT_MIN, ans2 = INT_MAX;
+    for(int i=0; i<n; i++){
+        maxi = max(maxi, a[i]);
+        mini = min(mini, a[i]);
+    }
+    for(int i=0; i<n; i++){
+        if(a[i]!=maxi){
+            ans1 = max(ans1, a[i]);
+        }
+    }
+    for(int i=0; i<n; i++){
+        if(a[i] != mini){
+            ans2 = min(ans2, a[i]);
+        }
+    }
+    vector<int> ans;
+    ans.push_back(ans1);
+    ans.push_back(ans2);
+    return ans;
+}
+
+int main(){
+    int n; cin>>n;
+    vector<int> arr(n);
+    for(int i=0; i<n; i++){
+        cin>>arr[i];
+    }
+    vector<int> ans = getSecondOrderElements(n, arr);
+    for(int i=0; i<ans.size(); i++){
+        cout<<ans[i]<<" ";
+    }
+    cout<<endl;
+}
