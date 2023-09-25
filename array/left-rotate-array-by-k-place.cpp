dff214eb9995e7967af2155ee85ba5d8e7bb6c20
Kumar Aryan <aryans6204@gmail.com>
2023-09-25 16:57:47 +0530

    rotate array by one place and k place

---
+++ array/left-rotate-array-by-k-place.cpp
@@ -0,0 +1,25 @@
+// Problem Link --> 
+
+#include <bits/stdc++.h> 
+using namespace std;
+
+vector<int> rotateArray(vector<int>arr, int k) {
+    // Write your code here.
+    reverse(arr.begin(), arr.begin()+k);
+    reverse(arr.begin()+k, arr.end());
+    reverse(arr.begin(), arr.end());
+    return arr;
+}
+
+int main(){
+    int n; cin>>n;
+    vector<int> arr(n);
+    for(int i=0; i<n; i++){
+        cin>>arr[i];
+    }
+    vector<int> ans = rotateArray(arr, n);
+    for(int i=0; i<n; i++){
+        cout<<ans[i]<<" ";
+    }
+    cout<<endl;
+}
---
+++ array/left-rotate-array-by-onePlace.cpp
@@ -0,0 +1,27 @@
+// Problem Link --> https://www.codingninjas.com/studio/problems/left-rotate-an-array-by-one_5026278?utm_source=striver&utm_medium=website&utm_campaign=a_zcoursetuf&leftPanelTab=0
+
+#include <bits/stdc++.h> 
+using namespace std;
+
+vector<int> rotateArray(vector<int>& arr, int n) {
+    // Write your code here.
+    int x = arr[0];
+    for(int i=0; i<n-1; i++){
+        arr[i] = arr[i+1];
+    }
+    arr[n-1] = x;
+    return arr;
+}
+
+int main(){
+    int n; cin>>n;
+    vector<int> arr(n);
+    for(int i=0; i<n; i++){
+        cin>>arr[i];
+    }
+    vector<int> ans = rotateArray(arr, n);
+    for(int i=0; i<n; i++){
+        cout<<ans[i]<<" ";
+    }
+    cout<<endl;
+}
