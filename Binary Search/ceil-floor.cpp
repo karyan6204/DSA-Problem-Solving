68676d388234e09c268ce52e396573490a392771
Kumar Aryan <aryans6204@gmail.com>
2023-10-01 15:12:26 +0530

    ceil and floor in an array

---
+++ Binary Search/ceil-floor.cpp
@@ -0,0 +1,48 @@
+// Problem Link -->
+#include<bits/stdc++.h>
+using namespace std;
+
+int getFloor(vector<int>& a ,int n, int x){
+	int ans = -1;
+	int low = 0, high = n-1;
+	while(low<=high){
+		int mid = low+(high-low)/2;
+		if(a[mid]<=x){
+			ans = a[mid];
+			low = mid+1;
+		}else{
+			high = mid-1;
+		}
+	}
+	return ans;
+}
+int getCeil(vector<int>& a ,int n, int x){
+	int ans = -1;
+	int low = 0, high = n-1;
+	while(low<=high){
+		int mid = low+(high-low)/2;
+		if(a[mid]>=x){
+			ans = a[mid];
+			high = mid-1;
+		}else{
+			low = mid+1;
+		}
+	}
+	return ans;
+}
+
+pair<int, int> getFloorAndCeil(vector<int> &a, int n, int x) {
+	// Write your code here.
+	pair<int,int> ans;
+	ans.first = getFloor(a, n, x);
+	ans.second = getCeil(a, n, x);
+	return ans;
+}
+
+int main(){
+    int n = 6, x = 8;
+    vector<int> arr = {3, 4, 4, 7, 8, 10};
+    pair<int,int> result = getFloorAndCeil(arr, n, x);
+    cout<<result.first<<" "<<result.second<<endl;
+    return 0;
+}
