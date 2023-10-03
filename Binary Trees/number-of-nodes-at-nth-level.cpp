c51b7c5fc5fdbab7348a4e1efbdde54c7425dbdf
Kumar Aryan <aryans6204@gmail.com>
2023-10-03 19:11:31 +0530

    number of nodes at any level in a binary tree

---
+++ Binary Trees/number-of-nodes-at-nth-level.cpp
@@ -0,0 +1,8 @@
+// Problem Link --> https://www.codingninjas.com/studio/problems/number-of-nodes_8162204
+#include<bits/stdc++.h>
+using namespace std;
+
+int numberOfNodes(int N){
+    // Write your code here.
+    return pow(2,N-1);
+}
